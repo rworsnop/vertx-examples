0ea7fe8e17f05ba0001d824f83a7a1efba092943
Rob Worsnop <RobWorsnop267@air-watch.com>
2015-09-02 10:26:17 -0400

    Expand Spring Boot example

--- spring-examples/springboot-example/src/main/java/io/vertx/example/Application.java
+++ spring-examples/springboot-example/src/main/java/io/vertx/example/Application.java
@@ -1,7 +1,13 @@
 package io.vertx.example;
 
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.Message;
+import io.vertx.ext.web.Router;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,16 +15,48 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    // This is basically the same example as the web-examples staticsite example but it's booted using
-    // SpringBoot, not Vert.x
-    SpringApplication.run(Application.class, args);
-  }
+        // This is basically the same example as the web-examples staticsite example but it's booted using
+        // SpringBoot, not Vert.x
+        SpringApplication.run(Application.class, args);
+    }
 
-  @Bean
-  public Vertx vertx(){
-      return Vertx.vertx();
-  }
+    @Bean
+    protected VertxOptions vertxOptions(@Value("${workerPoolSize:10}") int workerPoolSize){
+        VertxOptions options = new VertxOptions();
+        options.setWorkerPoolSize(workerPoolSize);
+        return options;
+    }
+
+    @Bean
+    public Vertx vertx(VertxOptions options) {
+        return Vertx.vertx(options);
+    }
+
+    @Bean
+    public Router router(Vertx vertx){
+        Router router = Router.router(vertx);
+
+        // /hello
+        router.get("/hello").handler(context -> context.response()
+                .end("Hello World from thread " + Thread.currentThread().getName() + "\n"));
+
+        // /timer?delay=1500
+        router.get("/timer").handler(context ->
+                vertx.setTimer(Integer.valueOf(context.request().params().get("delay")),
+                        id -> context.response().end(Thread.currentThread().getName() + "\n")));
+        // /bus?message=foo
+        router.get("/bus").handler(context ->{
+            vertx.eventBus().send("testaddress", context.request().params().get("message"), result -> {
+                if (result.succeeded()){
+                    context.response().end(result.result().body().toString()+"\n");
+                } else{
+                    context.fail(result.cause());
+                }
+            });
+        });
+        return router;
+    }
 
 }
---
+++ spring-examples/springboot-example/src/main/java/io/vertx/example/Server.java
@@ -0,0 +1,60 @@
+package io.vertx.example;
+
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
+import io.vertx.ext.web.Router;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+/**
+ * Created by rworsnop on 8/31/15.
+ */
+@Component
+public class Server {
+
+    private Vertx vertx;
+    private Router router;
+
+    private int port;
+    private int httpServerInstances;
+    private int busInstances;
+
+    @Value("${port:8080}")
+    public void setPort(int port) {
+        this.port = port;
+    }
+    @Value("${httpServers:1}")
+    public void setHttpServerInstances(int instances) {
+        this.httpServerInstances = instances;
+    }
+    @Value("${busInstances:1}")
+    public void setBusInstances(int instances) {
+        this.busInstances = instances;
+    }
+
+    @Resource
+    public void setRouter(Router router) {
+        this.router = router;
+    }
+
+    @Resource
+    public void setVertx(Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    @PostConstruct
+    public void createServer(){
+        for (int i =0; i < httpServerInstances; i++){
+            vertx.createHttpServer().requestHandler(router::accept).listen(port);
+        }
+        for (int i = 0; i < busInstances; i++){
+            vertx.eventBus().localConsumer("testaddress").handler(message ->
+                    message.reply("Processed " + message.body() + " in " + Thread.currentThread().getName()));
+
+        }
+    }
+}
